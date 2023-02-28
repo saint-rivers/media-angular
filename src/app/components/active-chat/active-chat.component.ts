8ef8e3df2aae40ef4103c61bf210bdd31b60f80f
dayan <eam.dayan@gmail.com>
2023-02-28 08:59:49 +0700

    feat(chat): routing between components

--- src/app/components/active-chat/active-chat.component.ts
+++ src/app/components/active-chat/active-chat.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-active-chat',
   templateUrl: './active-chat.component.html',
-  styleUrls: ['./active-chat.component.css']
+  styleUrls: ['./active-chat.component.css'],
 })
 export class ActiveChatComponent {
+  activeChatId: number = -1;
 
+  constructor(private router: ActivatedRoute) {
+    router.params.subscribe((pathParam) => {
+      this.activeChatId = parseInt(pathParam['id']);
+    });
+  }
 }
